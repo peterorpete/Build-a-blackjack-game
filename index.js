24c5a8f9ef2a59984d892f9d9652920836501ce0
Pete <peterorpete@me.com>
2021-08-27 11:23:11 +0100

    added css and html

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-// 1. Create two variables, firstCard and secondCard.
-// Set their values to a random number between 2-11
-
-// 2. Create a variable, sum, and set it to the sum of the two cards
-
-let firstCard = 17;
+let firstCard = 10;
 let secondCard = 4;
 let sum = firstCard + secondCard;
-let haBlackJack = false;
+let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById("message-el");
+// 2. Store the sum paragraph in a variable called sumEl
+let sumEl = document.getElementById("sum-el");
 
-if (sum <= 20) {
- message = "do you want to draw a new card";
-} else if (sum === 21) {
- message = "Wohoo you have got BlackJack!";
- haBlackJack = true;
-} else {
- message = "Your are out of the game";
- isAlive = false;
+function startGame() {
+ // 3. Render the sum on the page using this format -> "Sum: 14"
+ sumEl.textContent = "Sum: " + sum;
+ if (sum <= 20) {
+  message = "Do you want to draw a new card?";
+ } else if (sum === 21) {
+  message = "You've got Blackjack!";
+  hasBlackJack = true;
+ } else {
+  message = "You're out of the game!";
+  isAlive = false;
+ }
+ messageEl.textContent = message;
 }
-
-// Cash out!
-console.log(message);
