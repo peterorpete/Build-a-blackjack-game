57b5ae78aa07d4d407fc9f3282bef67dd55d80c8
Pete <peterorpete@me.com>
2021-09-10 12:10:34 +0100

    converted to an array the cards

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let firstCard = 10;
 let secondCard = 4;
+// 1. Create a new array - cards - that contains firstCard and secondCard
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
@@ -8,13 +10,13 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
-// Create a new function called startGame() that calls renderGame()
-
 function startGame() {
  renderGame();
 }
+
 function renderGame() {
- cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+ // 2. Refer to the cards array when rendering out the cards
+ cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
  sumEl.textContent = "Sum: " + sum;
  if (sum <= 20) {
   message = "Do you want to draw a new card?";
